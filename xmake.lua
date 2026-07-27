000fa4bf6723cdc9d7659ff4eadb13cfb7b6703c
Jinery <kychnooGames@gmail.com>
2026-07-27 11:07:09 +0400

    Initial commit: Hello world from EblanEngine.

---
+++ xmake.lua
@@ -0,0 +1,15 @@
+add_rules("mode.debug", "mode.release")
+set_languages("cxx20")
+
+add_requires("glfw", "glad")
+add_requires("opengl", {system = true, optional = true})
+
+add_rules("plugin.compile_commands.autoupdate", {lsp = "clangd"})
+
+target("EblanEngine")
+    set_kind("binary")
+    add_files("src/*.cpp")
+    add_files("src/**.cppm")
+    add_files("src/**.hpp")
+
+    add_packages("glfw", "glad", "opengl")
